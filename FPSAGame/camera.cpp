fef70eeb9ce7cbf8a8d2cd30c941b5d873152540
DESKTOP-89RG62R\takay <hzk770801@gmail.com>
2024-10-29 16:28:27 +0900

    2024/10/29 特になし

---
+++ FPSAGame/camera.cpp
@@ -0,0 +1,67 @@
+#include"camera.h"
+#include"DxLib.h"
+
+void Camera::CameraInitialize()
+{
+    CameraPos = VGet(INIT_CAMERA_X, INIT_CAMERA_Y, INIT_CAMERA_Z);
+
+    charaPosX = INIT_CHARACTOR_X;
+    charaPosY = INIT_CHARACTOR_Y;
+    charaPosZ = INIT_CHARACTOR_Z;
+
+    cameraPitch = INIT_SPIN;
+    cameraYaw   = INIT_SPIN;
+}
+
+void Camera::MouseInitialize()
+{
+    //マウスポインタの設定
+    GetMousePoint(&mousePosX, &mousePosY);
+    //マウスポインタ位置を画面中央に
+    SetMousePoint(INIT_MOUSE_X, INIT_MOUSE_Y);
+}
+
+void Camera::CameraMovement()
+{
+    //マウス操作に応じて視点を更新
+    cameraPitch -= (mousePosY - INIT_MOUSE_Y) * 0.01f;
+    cameraYaw   += (mousePosX - INIT_MOUSE_X) * 0.01f;
+
+    //上下回転の制限
+    if (cameraPitch >  DX_PI / 2)cameraPitch =  DX_PI / 2;
+    if (cameraPitch < -DX_PI / 2)cameraPitch = -DX_PI / 2;
+}
+
+void Camera::CharaMovement()
+{
+    //左移動
+    if (CheckHitKey(KEY_INPUT_A))
+    {
+        charaPosX -= 10.0f;
+    }
+    //前進
+    if (CheckHitKey(KEY_INPUT_W))
+    {
+        charaPosZ += 10.0f;
+    }
+    //後退
+    if (CheckHitKey(KEY_INPUT_S))
+    {
+        charaPosZ -= 10.0f;
+    }
+    //右移動
+    if (CheckHitKey(KEY_INPUT_D))
+    {
+        charaPosX += 10.0f;
+    }
+    if (CheckHitKey(KEY_INPUT_SPACE) && onGround)
+    {
+        charaPosY += 10.0f;
+        onGround = false;
+    }
+}
+
+void Camera::CameraUpdate()
+{
+    CameraPos = VGet(charaPosX, charaPosY, charaPosZ);
+}
---
+++ FPSAGame/camera.h
@@ -0,0 +1,70 @@
+#pragma once
+
+class Camera
+{
+private:
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ///　定数
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    const float INIT_CAMERA_X    = 0.0f;          //カメラX座標
+    const float INIT_CAMERA_Y    = 1.0f;          //カメラY座標
+    const float INIT_CAMERA_Z    = 0.0f;          //カメラZ座標
+
+    const float INIT_CHARACTOR_X = 0.0f;          //キャラクターX座標
+    const float INIT_CHARACTOR_Y = 0.0f;          //キャラクターY座標
+    const float INIT_CHARACTOR_Z = 0.0f;          //キャラクターZ座標
+
+    const float INIT_SPIN        = 0.0f;           //スピン初期
+
+    const int   INIT_MOUSE_X     = 1920 / 2;      //マウスX座標(画面中央)
+    const int   INIT_MOUSE_Y     = 1080 / 2;      //マウスY座標(画面中央)
+    
+    const float GRAV_ACC         = 9.8 / 10;      //重量加速度
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ///　変数
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    VECTOR CameraPos;   //カメラ位置
+
+    float charaPosX;    //キャラX座標
+    float charaPosY;    //キャラY座標
+    float charaPosZ;    //キャラZ座標
+
+    float cameraYaw;    //横回転
+    float cameraPitch;  //縦回転
+
+    int   mousePosX;      //マウス座標X
+    int   mousePosY;      //マウス座標Y
+
+    bool  onGround;      //地面にいるかどうか
+
+public:
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    ///　メソッド
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    /// <summary>
+    /// カメラの初期化
+    /// </summary>
+    void CameraInitialize();
+    /// <summary>
+    /// マウスの初期化
+    /// </summary>
+    void MouseInitialize();
+    /// <summary>
+    /// カメラの更新
+    /// </summary>
+    void CameraUpdate();
+    /// <summary>
+    /// 視点移動
+    /// </summary>
+    void CameraMovement();
+    /// <summary>
+    /// キャラクター移動
+    /// </summary>
+    void CharaMovement();
+};
